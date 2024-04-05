3ee37f4738811df84e4b28a8dcb79689bc41ab6e
TatyanaCheh <tatyana.cheh1@gmail.com>
2024-04-05 20:08:17 +0300

    тема4

--- js/task-4.js
+++ js/task-4.js
@@ -1,24 +1,39 @@
-function getShippingCost(country) {
-switch (country) {
-    case `China`:
-        return `Shipping to ${country} will cost 100 credits`;
-        break;
-        case `Chile`:
-            return `Shipping to ${country} will cost 250 credits`;
-            break;
-            case `Australia` :
-                return `Shipping to ${country} will cost 170 credits`; 
-                break;  
-                case `Jamaica` :
-                    return `Shipping to ${country} will cost 120 credits`;  
-                    break;
-                    default:
-                        return `Sorry, there is no delivery to your country`
-}
-}
-console.log(getShippingCost("Australia")); 
-console.log(getShippingCost("Germany")); 
-console.log(getShippingCost("China")); 
-console.log(getShippingCost("Chile")); 
-console.log(getShippingCost("Jamaica")); 
-console.log(getShippingCost("Sweden")); 
+
+/*const apartment = {
+    imgUrl: "https://via.placeholder.com/640x480",
+    descr: "Spacious apartment in the city center",
+    rating: 4,
+    price: 2153,
+    tags: ["premium", "promoted", "top"],
+    owner: {
+      name: "Henry",
+      phone: "982-126-1588",
+      email: "henry.carter@aptmail.com",
+    },
+  };
+  
+  const ownerName = apartment.owner.name;
+  const ownerPhone = apartment.owner.phone;
+  const ownerEmail = apartment.owner.email;
+  const numberOfTags = apartment.tags.length;
+  const firstTag = apartment.tags.slice(0, 1).join("");
+  const lastTag = apartment.tags.slice(-1);
+
+  console.log(numberOfTags);
+  console.log(firstTag);
+  console.log(lastTag);
+  */
+  const apartment = {
+    imgUrl: "https://via.placeholder.com/640x480",
+    descr: "Spacious apartment in the city center",
+    rating: 4,
+    price: 2153,
+    tags: ["premium", "promoted", "top"],
+  };
+  
+  const aptRating = apartment[price];
+  const aptDescr = apartment[descr];
+  const aptPrice = apartment[price];
+  const aptTags = apartment[tags];
+  console.log(aptTags);
+  
